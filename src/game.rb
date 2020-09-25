9b1939e9d2c72d7ad2e998270648a74cb8b442f5
Waldow <dwaldow@me.com>
2020-09-25 15:26:46 +1000

    Renamed play_game file and updated spec test

---
+++ src/game.rb
@@ -0,0 +1,10 @@
+class Game
+    def initialize
+        @player_name = ''
+        @wins = 0
+        @draws = 0
+        @score = 0
+        @date = #Today's Date
+        @time = #current time
+    end
+end
--- src/spec/play_game_spec.rb
+++ src/spec/play_game_spec.rb
@@ -1,4 +1,4 @@
-require_relative '../play_game'
+require_relative '../match'
 
 describe Match do
     it 'should return correct selection from array based on supplied index' do
@@ -7,12 +7,15 @@ describe Match do
         expect(subject.selections(2)).to eq 'ewok'
     end
 
-    it 'should have AI choice return random number between 0 and 2 inclusive' do
-        expect(subject.ai_choice_generator).to be_between(0, 2)
+    it 'should have ai_randomizer return random number between 0 and 2 inclusive' do
+        expect(subject.ai_randomizer).to be_between(0, 2)
     end
 
-    # it 'should have AI select random number between 0 and 2 inclusive' do
-    #     ai_choice_generator = Match.new
-    #     expect(ai_choice_generator)
+    # it 'should have @ai_selection save ai_choice' do
+    #     expect(subject.ai_choice('sith')).to eq('sith')
+    # end
+
+    # it 'should have @ai_selection save ai_choice' do
+    #     expect(subject.ai_choice_generator.selections.ai_choice.ai_selection).to eq('sith' || 'jedi' || 'ewok')
     # end
 end
