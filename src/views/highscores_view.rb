dacd31472483e0c78e34c4f5e02a4e2f60e67e95
waldowred5 <dwaldow@me.com>
2020-09-29 17:48:25 +1000

    Refactored Leaderboard code to be more DRY

--- src/views/highscores_view.rb
+++ src/views/highscores_view.rb
@@ -6,9 +6,9 @@ module HighscoresView
         puts "Leaderboard:".colorize(:light_yellow)
         return puts "\nNo scores on the leaderboard to show!".colorize(:red) if games.empty?
         headers = ['Player'.colorize(:light_blue), 'Score'.colorize(:light_magenta)]
-        all_scores = games.sort_by{|ary|ary[1]}.reverse
-        top10 = all_scores[0..9]
-        highscores = TTY::Table.new(header: headers, rows: top10)
+        # top_10 stores the highest 10 scores in YAML, sorted in descending order
+        top_10 = games.sort_by{|ary|ary[1]}.reverse[0..9]
+        highscores = TTY::Table.new(header: headers, rows: top_10)
         puts highscores.render :unicode, alignments: [:left, :right]
     end
 
