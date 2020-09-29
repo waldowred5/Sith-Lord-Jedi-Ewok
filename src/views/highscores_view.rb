bd435dba407871107666cf8295fd113a5ef3daf6
waldowred5 <dwaldow@me.com>
2020-09-29 17:43:54 +1000

    Updated Leaderboard to only show top 10 highest scores

--- src/views/highscores_view.rb
+++ src/views/highscores_view.rb
@@ -6,8 +6,9 @@ module HighscoresView
         puts "Leaderboard:".colorize(:light_yellow)
         return puts "\nNo scores on the leaderboard to show!".colorize(:red) if games.empty?
         headers = ['Player'.colorize(:light_blue), 'Score'.colorize(:light_magenta)]
-        rows = games.sort_by{|ary|ary[1]}.reverse
-        highscores = TTY::Table.new(header: headers, rows: rows)
+        all_scores = games.sort_by{|ary|ary[1]}.reverse
+        top10 = all_scores[0..9]
+        highscores = TTY::Table.new(header: headers, rows: top10)
         puts highscores.render :unicode, alignments: [:left, :right]
     end
 
